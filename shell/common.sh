e2152c4ddacb9fae7c8f48afc3c871323c8ed03e
danilojunS <danilojun9@gmail.com>
2016-01-16 20:46:40 -0200

    added bash config files

---
+++ shell/common.sh
@@ -0,0 +1,31 @@
+## Put here  are all common alias and scripts for bash and zsh shells
+
+## Detect OS
+
+OS='unknown'
+if [[ $(uname) == 'Darwin' ]]; then
+    OS='osx'
+elif [[ $(uname) == 'Linux' ]]; then
+    OS='linux'
+fi
+
+## Export ENV variables
+
+export PATH="/Users/danilojun/.bin:/Users/danilojun/.npm-packages/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin:/Volumes/HDD/Servers/glassfish4/bin"
+export JAVA_HOME=$(/usr/libexec/java_home)
+
+## ALIASES
+
+alias rs='reset'
+alias dotfiles='cd $HOME/.dotfiles'
+
+# git
+alias s='git status'
+alias p='git push'
+alias u='git pull'
+
+if [[ $OS == 'osx' ]]; then
+    alias blender="/Applications/Blender/blender.app/Contents/MacOS/blender"
+    alias sublime="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+fi
+
