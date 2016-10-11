450a8590fd8be7e9be6303ac4e4c23b2cb75641d
danilojunS <danilojun9@gmail.com>
2016-10-10 23:43:34 -0300

    added alias for vim

--- shell/common.sh
+++ shell/common.sh
@@ -26,6 +26,9 @@ alias s='git status'
 alias p='git push'
 alias u='git pull'
 
+# vim
+alias v='vim'
+
 if [[ $OS == 'osx' ]]; then
   alias blender="/Applications/Blender/blender.app/Contents/MacOS/blender"
   alias sublime="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
