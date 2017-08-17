cdc61af5da88f215e9f5a31a2c1cecdce2288d7e
Danilo Jun <danilojunS@users.noreply.github.com>
2017-08-17 00:25:55 -0300

    added script to install zsh plugins and source them (added new plugin zsh-autosuggestions)

---
+++ zsh/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+git clone git@github.com:zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
