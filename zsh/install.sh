70a5534a7d6300622da90ceba88a6ddfc292a628
Danilo Jun <danilojunS@users.noreply.github.com>
2017-08-17 00:44:23 -0300

    installing zsh plugins with antigen

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
-git clone git@github.com:zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+curl -L git.io/antigen > "$HOME/.oh-my-zsh/custom/plugins/antigen.zsh"
