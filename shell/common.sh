74e675edfa41c48637964138e28939a1fe2d84d9
Danilo Jun <danilojunS@users.noreply.github.com>
2017-09-25 12:16:03 -0300

    added vs code alias

--- shell/common.sh
+++ shell/common.sh
@@ -51,6 +51,9 @@ if [[ $OS == 'osx' ]]; then
   alias sublime="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 fi
 
+# vscode
+alias vs='code'
+
 # import packages and tools
 [[ ! "$(type fasd)" =~ "not found" ]] && eval "$(fasd --init auto)"
 [[ -f "$(brew --prefix)/etc/profile.d/z.sh" ]] && . `brew --prefix`/etc/profile.d/z.sh
